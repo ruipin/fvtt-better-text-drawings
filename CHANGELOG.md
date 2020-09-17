7d7b786c7234e60fb3fa9a1f0f3e779532c273dd
Rui Pinheiro <rui@ruipinheiro.eu>
2020-09-17 11:10:30 +0100

    Update libWrapper shim due to an upstream bug-fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.2 (2020-09-17)
+
+* Update libWrapper shim due to an upstream bug-fix.
+
 # 1.0.1 (2020-08-05)
 
 * Use libWrapper instead of a custom wrapper library.
--- shim.js
+++ shim.js
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright © 2020 fvtt-lib-wrapper Rui Pinheiro
 
-'use strict';
 
-// The browser doesn't expose all global variables (e.g. 'Game') inside globalThis, but it does to an eval
-const _libWrapperShim_eval = (__code) => eval(__code);
+'use strict';
 
 // A shim for the libWrapper library
 export let libWrapper = undefined;
@@ -20,28 +18,32 @@ Hooks.once('init', () => {
 	libWrapper = class {
 		static get is_fallback() { return true };
 
-		static register(module, target, fn) {
+		static register(module, target, fn, type="MIXED") {
 			const is_setter = target.endsWith('#set');
 			target = !is_setter ? target : target.slice(0, -4);
 			const split = target.split('.');
 			const fn_name = split.pop();
 			const root_nm = split.splice(0,1)[0];
-			const obj = split.reduce((x,y)=>x[y], globalThis[root_nm] ?? _libWrapperShim_eval(root_nm));
+			const _eval = eval; // The browser doesn't expose all global variables (e.g. 'Game') inside globalThis, but it does to an eval. We copy it to a variable to have it run in global scope.
+			const obj = split.reduce((x,y)=>x[y], globalThis[root_nm] ?? _eval(root_nm));
 
 			const descriptor = Object.getOwnPropertyDescriptor(obj, fn_name);
+
+			let original = null;
+			const wrapper = (type == 'OVERRIDE') ? function() { return fn.call(this, ...arguments); } : function() { return fn.call(this, original, ...arguments); }
 			if(descriptor.value) {
-				const original = obj[fn_name];
-				obj[fn_name] = function() { return fn.call(this, original, ...arguments); };
+				original = obj[fn_name];
+				obj[fn_name] = wrapper;
 				return;
 			}
 
 			if(!is_setter) {
-				let original = descriptor.get;
-				descriptor.get = function() { return fn.call(this, original, ...arguments); };
+				original = descriptor.get;
+				descriptor.get = wrapper;
 			}
 			else {
-				let original = descriptor.set;
-				descriptor.set = function() { return fn.call(this, original, ...arguments); };
+				original = descriptor.set;
+				descriptor.set = wrapper;
 			}
 			descriptor.configurable = true;
 			Object.defineProperty(obj, fn_name, descriptor);
